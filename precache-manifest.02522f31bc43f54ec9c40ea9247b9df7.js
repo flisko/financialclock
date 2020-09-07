cb9d435a925dab16f245bf02352b4dff6a6b14b6
flisko <ziga.flis@student.um.si>
2020-09-07 11:12:31 +0200

    Updates

---
+++ precache-manifest.02522f31bc43f54ec9c40ea9247b9df7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e7e823b7a8c8f2db8f9edefb7ac755f",
+    "url": "/financialclock/index.html"
+  },
+  {
+    "revision": "528e0f06affb02af7b37",
+    "url": "/financialclock/static/css/2.3807f13a.chunk.css"
+  },
+  {
+    "revision": "c6576f5bfa441ce2a5e5",
+    "url": "/financialclock/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "528e0f06affb02af7b37",
+    "url": "/financialclock/static/js/2.f5851c5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/financialclock/static/js/2.f5851c5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6576f5bfa441ce2a5e5",
+    "url": "/financialclock/static/js/main.a44d3932.chunk.js"
+  },
+  {
+    "revision": "08bb0c7a13959981a6ba",
+    "url": "/financialclock/static/js/runtime-main.9a2ce402.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/financialclock/static/media/logo.5d5d9eef.svg"
+  }
+]);
